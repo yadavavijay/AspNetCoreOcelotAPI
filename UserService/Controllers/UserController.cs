99cca9e43b988645651faad0dd5605a9e45d698b
Vijay Yadav <36539650+vijayyadava@users.noreply.github.com>
2021-04-29 12:42:03 +0530

    Add files via upload

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        DatabaseContext db;
+        public UserController()
+        {
+            db = new DatabaseContext();
+        }
+        // GET: api/user
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return db.Users.ToList();
+        }
+
+        // GET api/user
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        // POST api/user
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=localhost; initial catalog=UserMicroservice; persist security info=True; user id=sa; password=xxxxxx;");
+
+        }
+    }
+}
